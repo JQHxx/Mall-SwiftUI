2b8f1af38da396ec75dd240668f5983e4f85ddb6
HJQ <>
2021-05-19 15:21:47 +0800

    提交

---
+++ SwiftUI_Jike/Pods/Target Support Files/Pods-SwiftUI_Jike/Pods-SwiftUI_Jike-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SwiftUI_Jike : NSObject
+@end
+@implementation PodsDummy_Pods_SwiftUI_Jike
+@end
--- SwiftUI_Jike/SwiftUI_Jike/Views/ContentView.swift
+++ SwiftUI_Jike/SwiftUI_Jike/Views/ContentView.swift
@@ -7,18 +7,22 @@
 //
 
 import SwiftUI
+import struct Kingfisher.KFImage
 
 struct ContentView : View {
 	@State var currentPage = 0
 	let segItems: [String] = ["关注", "推荐", "附近", "即刻合伙人"]
     var body: some View {
-		GYTabView(views:
-					[AnyView(HomeView()),
-					 AnyView(ActivityView()),
-					 AnyView(ChatView()),
-					 AnyView(SettingView())],
-				  tabItems: DataMgr.shared.getTabItems())
-			.edgesIgnoringSafeArea(.top)
+//		GYTabView(views:
+//					[AnyView(HomeView()),
+//					 AnyView(ActivityView()),
+//					 AnyView(ChatView()),
+//					 AnyView(SettingView())],
+//				  tabItems: DataMgr.shared.getTabItems())
+//			.edgesIgnoringSafeArea(.top)
+		
+		KFImage(URL(string: "https://is2-ssl.mzstatic.com/image/thumb/Purple124/v4/a7/32/8c/a7328c6d-247e-578e-64fb-666ba3990947/source/512x512bb.jpg")!)
+
 
     }
 }
